f98b28e77ac37354c873c8732fd03aaac00e0103
Luis Enrique Sosa Hernández <kiquesasuke@gmail.com>
2020-08-07 15:21:04 -0500

    Login

--- 07-restserver/server/config/config.js
+++ 07-restserver/server/config/config.js
@@ -5,6 +5,12 @@ process.env.PORT =  process.env.PORT || 3000;
 
     process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
+
+    //Vencimiento token
+    process.env.CADUCIDAD_TOKEN= 60*60*24*30; //60 SEGUNDOS 60 MINUTOS 24 HORAS 30 DIAS
+
+    //SEED de autentication
+    process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
     //Base de datos
 
     let urlDB;
--- 07-restserver/server/routes/usuario.js
+++ 07-restserver/server/routes/usuario.js
@@ -1,62 +1,66 @@
 const express = require('express')
 const bcrypt = require('bcrypt');
 const Usuario = require('../../models/usuario');
+const { verificaToken,verificaAdminRole } = require('../middlewares/autenticacion')
 const _ = require('underscore');
-const { request } = require('express');
-const usuario = require('../../models/usuario');
 
 const app = express()
-app.get('/usuario', function (req, res) {
+app.get('/usuario', verificaToken, (req, res) => {
+    return res.json({
+        usuario: req.usuario,
+        nombre: req.usuario.nombre,
+        email: req.usuario.email
+    })
     let desde = req.query.desde || 0;
-    desde=Number(desde);
+    desde = Number(desde);
     let limite = req.query.limite || 5;
-    limite=  Number(limite);
-    Usuario.find({estado:true},'nombre email role estado google img').skip(desde).limit(limite)
-    .exec((err,usuarios)=>{
-        if (err) {
-            return res.status(400).json({
-                ok:false,
-                err
-            })
-        }
-
-        Usuario.count({estado:true},(err,conteo)=>{
+    limite = Number(limite);
+    Usuario.find({ estado: true }, 'nombre email role estado google img').skip(desde).limit(limite)
+        .exec((err, usuarios) => {
             if (err) {
                 return res.status(400).json({
-                    ok:false,
+                    ok: false,
                     err
                 })
             }
 
-            res.json({
-                ok:true,
-                usuarios,
-                conteo
-            })
-        });
+            Usuario.countDocuments({ estado: true }, (err, conteo) => {
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    })
+                }
 
-    })
+                res.json({
+                    ok: true,
+                    usuarios,
+                    conteo
+                })
+            });
+
+        })
 })
 
-app.post('/usuario', function (req, res) {
+app.post('/usuario',[verificaToken,verificaAdminRole], function (req, res) {
     let body = req.body;
 
     let usuario = new Usuario({
         nombre: body.nombre,
         email: body.email,
-        password: bcrypt.hashSync(body.password,10),
+        password: bcrypt.hashSync(body.password, 10),
         role: body.role
     })
 
     usuario.save((err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
-                ok:false,
+                ok: false,
                 err
             })
         }
 
-        
+
         res.json({
             ok: true,
             usuario: usuarioDB
@@ -65,51 +69,51 @@ app.post('/usuario', function (req, res) {
     })
 })
 
-app.put('/usuario/:id', function (req, res) {
+app.put('/usuario/:id',[verificaToken,verificaAdminRole], function (req, res) {
     let id = req.params.id;
-    let body= _.pick(req.body,['nombre','email','img','role','estado']);
-    Usuario.findByIdAndUpdate(id,body,{new:true,runValidators:true},(err,usuarioDB)=>{
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
         if (err) {
             return res.status(400).json({
-                ok:false,
+                ok: false,
                 err
             })
         }
 
         res.json({
-            ok:true,
-            usuario:usuarioDB
-        })     
+            ok: true,
+            usuario: usuarioDB
+        })
     })
-   
+
 })
 
-app.delete('/usuario/:id', function (req, res) {
-   let id = req.params.id;
-   let cambiaEstado = {
-       estado:false
-   }
-//    Usuario.findByIdAndRemove(id,(err,usuarioBorrado)=>{
-    Usuario.findByIdAndUpdate(id,cambiaEstado,{new:true},(err,usuarioBorrado)=>{
-    if (err) {
-        return res.status(400).json({
-            ok:false,
-            err
-        })
+app.delete('/usuario/:id',[verificaToken,verificaAdminRole], function (req, res) {
+    let id = req.params.id;
+    let cambiaEstado = {
+        estado: false
     }
-    if(!usuarioBorrado){
-        return res.status(400).json({
-            ok:false,
-            err:{
-                message: 'Usuario no encontrado'
-            }
+    //    Usuario.findByIdAndRemove(id,(err,usuarioBorrado)=>{
+    Usuario.findByIdAndUpdate(id, cambiaEstado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            })
+        }
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                err: {
+                    message: 'Usuario no encontrado'
+                }
+            })
+        }
+        res.json({
+            ok: true,
+            usuarioBorrado
         })
-    }
-    res.json({
-        ok:true,
-        usuarioBorrado
     })
-   })
 })
 
 module.exports = app;
